a44b380f0801d5e942aebad3fc2c58d71237b68b
kevin12081 <kevin90298@gmail.com>
2022-11-16 16:07:54 +0800

    製作網站Navbar & 篩選欄

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
 import "./index.scss";
+import Carousel from "bootstrap/js/dist/carousel";
+import Offcanvas from "bootstrap/js/dist/offcanvas";
+import Collapse from "bootstrap/js/dist/collapse";
+
+// ------電腦版-------
+function collapsePCHandler() {
+  // get elements
+  const collapse = document.getElementById("filter-collapse");
+  const collapseBtn = document.getElementById("filter-collapse-btn");
+  const collapseBtnText = document.getElementById("filter-btn-text");
+  const filterIcon = document.getElementById("filter-icon");
+
+  const bsCollapse = new Collapse(collapse, {
+    toggle: false,
+  });
+
+  collapseBtn.addEventListener("click", function () {
+    bsCollapse.toggle();
+  });
+
+  collapse.addEventListener("show.bs.collapse", function () {
+    collapseBtnText.innerText = "收起";
+    filterIcon.classList.add("transform-rotate-180");
+  });
+  collapse.addEventListener("hide.bs.collapse", function () {
+    collapseBtnText.innerText = "顯示更多";
+    filterIcon.classList.remove("transform-rotate-180");
+  });
+}
+
+// --------手機版----------
+function collapseMobileHandler() {
+  const collapseMobile = document.getElementById("filter-mobile-collapse");
+  const collapseBtnMobile = document.getElementById(
+    "filter-mobile-collapse-btn"
+  );
+  const collapseBtnTextMobile = document.getElementById(
+    "filter-mobile-btn-text"
+  );
+  const filterIconMobile = document.getElementById("filter-mobile-icon");
+
+  // 初始化下拉選單
+
+  const bsCollapseMobile = new Collapse(collapseMobile, {
+    toggle: false,
+  });
+
+  collapseBtnMobile.addEventListener("click", function () {
+    bsCollapseMobile.toggle();
+  });
+
+  collapseMobile.addEventListener("show.bs.collapse", function () {
+    collapseBtnTextMobile.innerText = "收起";
+    filterIconMobile.classList.add("transform-rotate-180");
+  });
+  collapseMobile.addEventListener("hide.bs.collapse", function () {
+    collapseBtnTextMobile.innerText = "顯示更多";
+    filterIconMobile.classList.remove("transform-rotate-180");
+  });
+}
+
+// -----控制offcanvas------
+function offcanvasHandler() {
+  const myOffcanvas = document.getElementById("offcanvasScrolling");
+  const mobileFilterBtn = document.getElementById("mobile-filter-btn");
+
+  const bsOffcanvas = new Offcanvas(myOffcanvas, { backdrop: true });
+
+  mobileFilterBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  });
+}
+
+collapsePCHandler();
+collapseMobileHandler();
+offcanvasHandler();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  target: 'web',
-  entry: './src/index.js',
-  mode: process.env === 'development' ? 'development' : 'production',
+  target: "web",
+  entry: "./src/index.js",
+  mode: process.env === "development" ? "development" : "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[hash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[hash].js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
   module: {
     rules: [
@@ -18,35 +23,37 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.(css|scss)$/i,
         exclude: /node_modules/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
           "postcss-loader",
-          "sass-loader"       
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.svg/,
-        type: 'asset/inline'
-      }
-    ]
+        type: "asset/inline",
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-        filename: 'index.[hash].css'
+      filename: "index.[hash].css",
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: "./src/index.html",
+    }),
   ],
   devServer: {
     open: true,
-  }
-}
+    hot: false,
+    liveReload: true,
+  },
+};
